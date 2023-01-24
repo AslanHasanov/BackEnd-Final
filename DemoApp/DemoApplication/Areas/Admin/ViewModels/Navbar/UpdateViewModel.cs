7ccd3f64ce937f5057adfc55c40567452b1a759d
AslanHasanov <aslanrh@code.edu.az>
2023-01-24 16:03:24 +0400

    Navbar CRUD succesfully completed

---
+++ DemoApp/DemoApplication/Areas/Admin/ViewModels/Navbar/UpdateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoApplication.Areas.Admin.ViewModels.Navbar
+{
+    public class UpdateViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public int Order { get; set; }
+
+        [Required]
+        public string? URL { get; set; }
+ 
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
