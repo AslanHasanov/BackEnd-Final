def6060102c1a2f7ff7ab5df4a95e070849b4aeb
AslanHasanov <aslanrh@code.edu.az>
2023-01-26 19:05:08 +0400

    Modal functionality succesfully compleeted

--- DemoApp/DemoApplication/wwwroot/Client/assets/js/QuickModal.js
+++ DemoApp/DemoApplication/wwwroot/Client/assets/js/QuickModal.js
@@ -1,24 +1,21 @@
-﻿$(document).on("click", ".show-product-modal", function (e) {
+﻿
+$('.a-tag li a').filter(function () {
+    return this.href === location.href;
+}).addClass('active');
+
+$(document).on("click", ".quuickview-btn", function (e) {
     e.preventDefault();
 
-    console.log(e.target)
-
     var url = e.target.parentElement.href;
-
     console.log(url)
 
     fetch(url)
         .then(response => response.text())
         .then(data => {
-            $('.product-details-modal').html(data);
+
+            $('.modalProduct').html(data);
             console.log(data)
         })
 
     $("#quickModal").modal("show");
 })
-
-
-
-$('.a-tag li a').filter(function () {
-    return this.href === location.href;
-}).addClass('active');
