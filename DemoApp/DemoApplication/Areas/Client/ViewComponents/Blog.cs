3aa2ddf7ec74e40853da5fce46ace7aaa37a20d3
AslanHasanov <aslanrh@code.edu.az>
2023-02-03 03:49:22 +0400

    Client Blog and BlogPage added to system

---
+++ DemoApp/DemoApplication/Areas/Client/ViewComponents/Blog.cs
@@ -0,0 +1,44 @@
+﻿using DemoApplication.Areas.Client.ViewModels.Home;
+using DemoApplication.Database;
+using DemoApplication.Services.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace DemoApplication.Areas.Client.ViewComponents
+{
+    [ViewComponent(Name = "Blog")]
+
+    public class Blog : ViewComponent
+    {
+        private readonly DataContext _dataContext;
+        private readonly IFileService _fileService;
+
+        public Blog(DataContext dataContext, IFileService fileService)
+        {
+            _dataContext = dataContext;
+            _fileService = fileService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model =
+               await _dataContext.Blogs
+               .Include(b => b.BlogAndTags).Include(b => b.BlogAndCategories).Include(b => b.BlogDisplays)
+                .Select(b => new BlogViewModel(b.Id, b.Title, b.Content,
+                b.BlogDisplays!.Take(1).FirstOrDefault() != null
+                ? _fileService.GetFileUrl(b.BlogDisplays.Take(1).FirstOrDefault()!.FileNameInSystem, Contracts.File.UploadDirectory.Blog)
+                : String.Empty,
+                b.BlogDisplays!.FirstOrDefault()!.IsImage,
+                b.BlogDisplays!.FirstOrDefault()!.IsVidio,
+                b.CreatedAt,
+                b.BlogAndTags!.Select(b => b.Tag).Select(b => new BlogViewModel.TagViewModel(b.Title)).ToList(),
+                b.BlogAndCategories!.Select(b => b.Category)
+                .Select(b => new BlogViewModel.CategoryViewModeL(b.Title, b.Parent!.Title)).ToList()
+                )).ToListAsync();
+
+            return View(model);
+        }
+    }
+}
---
+++ DemoApp/DemoApplication/Areas/Client/ViewModels/Home/BlogViewModel.cs
@@ -0,0 +1,55 @@
+﻿namespace DemoApplication.Areas.Client.ViewModels.Home
+{
+    public class BlogViewModel
+    {
+       
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string BlogFiles { get; set; }
+        public bool IsImage { get; set; }
+        public bool IsVideo { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public List<TagViewModel> Tags { get; set; }
+        public List<CategoryViewModeL> Categories { get; set; }
+
+        public BlogViewModel(int id, string title, string content, string blogFiles, bool isImage, bool isVideo,
+            DateTime createdAt, List<TagViewModel> tags, List<CategoryViewModeL> categories)
+        {
+            Id = id;
+            Title = title;
+            Content = content;
+            BlogFiles = blogFiles;
+            IsImage = isImage;
+            IsVideo = isVideo;
+            CreatedAt = createdAt;
+            Tags = tags;
+            Categories = categories;
+        }
+
+        public class TagViewModel
+        {
+            public TagViewModel(string title)
+            {
+                Title = title;
+            }
+
+            public string Title { get; set; }
+        }
+        public class CategoryViewModeL
+        {
+            public CategoryViewModeL(string title, string parentTitle)
+            {
+                Title = title;
+                ParentTitle = parentTitle;
+            }
+
+            public string Title { get; set; }
+            public string ParentTitle { get; set; }
+
+
+        }
+    }
+}
