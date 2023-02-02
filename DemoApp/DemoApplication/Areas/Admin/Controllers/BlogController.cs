b55a2ce96648aaaf106e68b83d8cc590026fc6ed
AslanHasanov <aslanrh@code.edu.az>
2023-02-02 18:25:03 +0400

    Blog Controller completed

--- DemoApp/DemoApplication/Areas/Admin/Controllers/BlogController.cs
+++ DemoApp/DemoApplication/Areas/Admin/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using DemoApplication.Areas.Admin.ViewModels.Blog;
 using DemoApplication.Database;
+using DemoApplication.Database.Models;
 using DemoApplication.Services.Abstracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -32,16 +33,20 @@ namespace DemoApplication.Areas.Admin.Controllers
 
             var model = await _dataContext.Blogs.OrderByDescending(p => p.CreatedAt)
                 .Select(p => new ListItemViewModel(p.Id, p.Title, p.Content, p.CreatedAt,
-                p.BlogAndTags.Select(ps => ps.Tag)
+                p.BlogAndTags!.Select(ps => ps.Tag)
                 .Select(s => new ListItemViewModel.TagViewModel(s.Title)).ToList(),
-                p.BlogAndCategories.Select(pc => pc.Category)
-                .Select(c => new ListItemViewModel.CategoryViewModeL(c.Title, c.Parent.Title)).ToList()
+                p.BlogAndCategories!.Select(pc => pc.Category)
+                .Select(c => new ListItemViewModel.CategoryViewModeL(c.Title, c.Parent!.Title)).ToList()
               
                 )).ToListAsync();
 
 
             return View(model);
         }
+
+        #endregion
+
+        #region Add' 
 
         [HttpGet("add", Name = "admin-blog-add")]
         public async Task<IActionResult> Add()
@@ -52,12 +57,128 @@ namespace DemoApplication.Areas.Admin.Controllers
                     .Select(c => new BlogCategoryViewModel(c.Id, c.Title))
                     .ToListAsync(),
                 Tags = await _dataContext.BlogTags
-                .Select(t => new BlogTagViewModel(t.Id, t.Title))
-                .ToListAsync()
+                    .Select(t => new BlogTagViewModel(t.Id, t.Title))
+                    .ToListAsync()
             };
 
             return View(model);
         }
+
+        [HttpPost("add", Name = "admin-blog-add")]
+        public async Task<IActionResult> Add(AddViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return GetView(model);
+            }
+
+            foreach (var categoryId in model.CategoryIds)
+            {
+                if (!await _dataContext.BlogCategories.AnyAsync(c => c.Id == categoryId))
+                {
+                    ModelState.AddModelError(string.Empty, "Something went wrong");
+                    _logger.LogWarning($"Category with ({categoryId}) not found in database ");
+                    return GetView(model);
+                }
+
+            }
+
+            foreach (var tagId in model.TagIds)
+            {
+                if (!await _dataContext.BlogTags.AnyAsync(c => c.Id == tagId))
+                {
+                    ModelState.AddModelError(string.Empty, "Something went wrong");
+                    _logger.LogWarning($"Tag with ({tagId}) not found in database ");
+                    return GetView(model);
+                }
+
+            }
+
+
+
+            AddProduct();
+
+            await _dataContext.SaveChangesAsync();
+
+            return RedirectToRoute("admin-blog-list");
+
+
+
+            IActionResult GetView(AddViewModel model)
+            {
+
+                model.Categories = _dataContext.BlogCategories
+                   .Select(c => new BlogCategoryViewModel(c.Id, c.Title))
+                   .ToList();
+
+                model.Tags = _dataContext.BlogTags
+                 .Select(c => new BlogTagViewModel(c.Id, c.Title))
+                 .ToList();
+
+
+                return View(model);
+            }
+
+
+            async void AddProduct()
+            {
+                var product = new Blog
+                {
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedAt = DateTime.Now,
+                };
+
+
+                await _dataContext.Blogs.AddAsync(product);
+
+
+                foreach (var catagoryId in model.CategoryIds)
+                {
+                    var productCatagory = new BlogAndBlogCategory
+                    {
+                        BlogCategoryId = catagoryId,
+                        Blog = product,
+                    };
+
+                    await _dataContext.BlogAndBlogCategories.AddAsync(productCatagory);
+                }
+
+                foreach (var tagId in model.TagIds)
+                {
+                    var productTag = new BlogAndBlogTag
+
+                    {
+                        BlogTagId = tagId,
+                        Blog = product
+
+                    };
+
+                    await _dataContext.BlogAndBlogTags.AddAsync(productTag);
+                }
+
+
+            }
+        } 
+        #endregion
+
+
+        #region Delete'
+
+        [HttpPost("delete/{id}", Name = "admin-blog-delete")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int id)
+        {
+            var products = await _dataContext.Blogs.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (products is null) return NotFound();
+           
+
+            _dataContext.Blogs.Remove(products);
+            await _dataContext.SaveChangesAsync();
+            return RedirectToRoute("admin-blog-list");
+        }
+
+
         #endregion
     }
 }
